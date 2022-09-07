53cee4c926a6ad0a27ee604f8169d73fa6f62e88
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-09-07 12:18:54 +0530

    added event detail page

--- section-4-nextevents-routing-proj/pages/events/[eventId].js
+++ section-4-nextevents-routing-proj/pages/events/[eventId].js
@@ -1,8 +1,33 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const eventDetails = getEventById(eventId);
+  if (!eventDetails) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Detail page for a single event.</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={eventDetails.title} />
+      <EventLogistics
+        date={eventDetails.date}
+        address={eventDetails.location}
+        image={eventDetails.image}
+        imageAlt={eventDetails.title}
+      />
+      <EventContent>
+        <p>{eventDetails.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
